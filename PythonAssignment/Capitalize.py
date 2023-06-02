ea44d4a7e485211cec242a504e86f6e4e6e0ec67
Dhruvil Gadoya <gadoyadhruvil0@gmail.com>
2023-06-02 15:20:40 +0530

    Added python questions files

---
+++ PythonAssignment/Capitalize.py
@@ -0,0 +1,25 @@
+'''
+You are asked to ensure that the first and last names of people begin with a capital letter in their passports.
+For example, alison heck should be capitalised correctly as Alison Heck.
+'''
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def solve(s):
+    "Write your logic here."
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = solve(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
