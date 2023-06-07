e8b39b4996feb0977c916c9705c1702435ea277a
Dhruvil Gadoya <gadoyadhruvil0@gmail.com>
2023-06-07 12:48:33 +0530

    changes

--- PythonAssignment/SumOfTwo.py
+++ PythonAssignment/SumOfTwo.py
@@ -11,3 +11,13 @@ class Solution(object):
         """
         Write your logic here.
         """
+        a = []
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if (nums[i] + nums[j] == target):
+                    a.append(i)
+                    a.append(j)
+                    break
+                else:
+                    continue
+        return a
