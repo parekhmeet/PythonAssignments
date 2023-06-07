ebe9909b53bcca70bfc99a096005f72296e56ee8
Dhruvil Gadoya <gadoyadhruvil0@gmail.com>
2023-06-07 17:29:11 +0530

    changes

--- PythonAssignment/SumOfTwo.py
+++ PythonAssignment/SumOfTwo.py
@@ -6,7 +6,8 @@ Output: [0,1]
 Explanation: Because nums[0] + nums[1] == 4, we return [0, 1]. """
 
 class Solution(object):
-    def SumOfTwo(self, nums, target):
+    def SumOfTwo(self, nums, target):\
+
         """
         Write your logic here.
         """
