c217fdd0a3ed25609508974b7091fc4aa9b7753f
Meet Parekh <50916854+parekhmeet@users.noreply.github.com>
2023-06-17 19:26:37 +0530

    Update Capitalize.py

--- PythonAssignment/Capitalize.py
+++ PythonAssignment/Capitalize.py
@@ -1,8 +1,8 @@
-'''
+'''fesf
 You are asked to ensure that the first and last names of people begin with a capital letter in their passports.
 For example, alison heck should be capitalised correctly as Alison Heck.
 '''
-
+fs
 
 import math
 import os
