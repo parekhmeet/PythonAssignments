37b6c5de559559c6e54a4113e2e0245653510759
Meet Parekh <50916854+parekhmeet@users.noreply.github.com>
2023-06-19 17:58:14 +0530

    Update ValidPalindrome.py

--- PythonAssignment/ValidPalindrome.py
+++ PythonAssignment/ValidPalindrome.py
@@ -1,7 +1,7 @@
 '''
 A phrase is a palindrome if, after converting all uppercase letters into
 lowercase letters and removing all non-alphanumeric characters,
-it reads the same forward and backward. Alphanumeric characters include
+it reads the same forward and backward. Alphanu
 letters and numbers.
 
 Input: s = "A man, a plan, a canal: Panama"
