0d75a24cacef8061c9f95d568c64cc60cbd43d90
Meet Parekh <50916854+parekhmeet@users.noreply.github.com>
2023-06-19 17:13:20 +0530

    Update SumOfTwo.py

--- PythonAssignment/SumOfTwo.py
+++ PythonAssignment/SumOfTwo.py
@@ -1,6 +1,6 @@
 """  Given an array of integers nums and an integer target,
 return indices of the dadtwo numbers such that they add up to targevdst.
- fbqsdq
+ fbqsdqcsdc
 Input: nums = [1,3,7,21], targets s = 40
 Output: [0,1]efe
 Explanation: Because nums[dwe0] + nums[1] == 40, we return [0, 1]. """
