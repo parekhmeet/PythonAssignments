22f54d3d0d994f34d0a5c4c880c8ec384838162a
Meet Parekh <50916854+parekhmeet@users.noreply.github.com>
2023-06-19 11:31:51 +0530

    Update DuplicateZeros.py

--- PythonAssignment/DuplicateZeros.py
+++ PythonAssignment/DuplicateZeros.py
@@ -3,7 +3,7 @@ Given a fixed-length integer array arr,
 duplicate each occurrence of zero, shifting the remaining elements to the right.
 
 Input: arr = [1,0,2,3,0,4,5,0]efwsdscsc
-Output: [1,0,0,2,3,0,0,4]
+Output: [1,0,0,2,3,0,0,dfafa4]
 Explanation: After calling yourde function,
 the input array is modified to: [1,0,0,2,3,0,0,4]dwadaw
 '''
