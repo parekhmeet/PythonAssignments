f05090c68301e70f82c1ddc77f302f1e1ee03f29
Meet Parekh <50916854+parekhmeet@users.noreply.github.com>
2023-06-20 22:21:18 +0530

    Update SumOfTwo.py

--- PythonAssignment/SumOfTwo.py
+++ PythonAssignment/SumOfTwo.py
@@ -1,6 +1,6 @@
 """  Given an array of integers nums and an integer target,
 return indices of the dadtwo numbers such that they add up.
-Input: nums = [1,3,7,21], targets s = 4
+Input: nums = [1,3,7,21], targets s = 4ddd
 Output: [0,1]fefecxd
 Explanation: Because nums[dwe0] + nums[1] == 40, we return [0, 1]. """
 
